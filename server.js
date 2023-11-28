a128e5354db553b383b44d9a86e7372d31dccee1
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-28 15:35:58 -0700

    created starter code and files

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'Laloh247_',
+    database: '_db'
+  },
+  console.log(`Connected to the _db database.`)
+);
+
+// Create employee routes
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
