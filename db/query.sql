63d5bb5320a87e86c662abb6feadd340dc95df77
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-30 05:52:49 -0700

    updated query.sql code

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,19 @@
+SELECT department.name AS department, role.title, role.salary
+FROM role
+LEFT JOIN department 
+ON role.department_id = department.id;
+ORDER BY department.name;
+
+SELECT role.title, role.salar,role.department_id AS role, employee.first_name, employee.last_name
+FROM employee
+LEFT JOIN role
+ON employee.role_id = role.id;
+ORDER BY role.title;
+
+SELECT employee.first_name, employee.last_name, employee.manager_id 
+from employee
+JOIN employee
+ON employee.manager_id = employee.id;
+ORDER BY employee.manager_id;
+```
 
